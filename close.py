c4965c33e0b498adb625289238a2c17be63b8855
Carson C <62458142+CrabbleOrNiceTry@users.noreply.github.com>
2020-12-04 00:01:21 -0700

    Update close.py

--- close.py
+++ close.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ChromeOptions
-import random
-import re
 import time
 import sys
 
